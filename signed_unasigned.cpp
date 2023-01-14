6bd1c373cd95c86598d4c9424bae37fcf7a06700
Ahmad Faraz <ahmadfarazjanjua780@gmail.com>
2023-01-14 23:50:09 +0500

    Signed and unSigned difference

---
+++ signed_unasigned.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    unsigned int anUnsignedInt; // 0 to 4,294,967,295
+    unsigned short anUnsignedShort; // 0 to 65535
+    signed long aSignedLong; // -2,147,483,648 to 2,147,483,647
+    
+}
