0bc56c04721936439455e1575e747772235b01e9
Ahmad Faraz <ahmadfarazjanjua780@gmail.com>
2023-03-19 20:34:46 +0500

    just little about memory location

---
+++ pointers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+// output the value of the variable
+// as well as the memory address of the variable in c++
+int main()
+{
+    int varN = 101;
+
+    cout << varN << endl;
+
+    cout << &varN << endl;
+
+    return 0;
+}
