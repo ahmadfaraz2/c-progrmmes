53493d0ea89f9196e193219baccb0daaac8c9af7
Ahmad Faraz <ahmadfarazjanjua780@gmail.com>
2023-03-19 22:59:01 +0500

    learn about recursive function and factorial

---
+++ recursive_func_call.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial(n - 1);
+    }
+}
+
+int main()
+{
+    int num = 5;
+    int result = factorial(num);
+    cout << "The factorial of " << num << " is " << result << endl;
+    return 0;
+}
